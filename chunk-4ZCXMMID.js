b1200fc7b9416157c8d2e543e1431de1daff84ef
Sobhit Kumar <sobhit2010@gmail.com>
2026-02-05 09:24:18 +0000

    Auto-generated commit

---
+++ chunk-4ZCXMMID.js
@@ -0,0 +1,1 @@
+import{Fb as p,Ya as o,ob as n,pb as a}from"./chunk-4CABIFJF.js";var i=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(n(0,"p"),p(1,"about works!"),a())},encapsulation:2})};export{i as AboutComponent};
